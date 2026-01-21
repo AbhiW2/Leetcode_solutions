351de9097ce2ba62987486d5d690ac88f453fa86
Abhi Wargad <wargadabhi@gmail.com>
2026-01-21 16:17:50 +0530

    Reverse

--- 7_Reverse_integer.cpp
+++ 7_Reverse_integer.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <climits> // For INT_MAX and INT_MIN
+#include <climits> 
 using namespace std;
 
-// LeetCode Solution Class
+
 class Solution {
 public:
     int reverse(int x) {
@@ -12,7 +12,7 @@ public:
             int digit = x % 10;
             x /= 10;
 
-            // Check for overflow before multiplying by 10
+            
             if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
                 return 0;
             if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
@@ -24,13 +24,13 @@ public:
     }
 };
 
-// Main function for local testing
+
 int main() {
     Solution sol;
 
     int test1 = 123;
     int test2 = -123;
-    int test3 = 1534236469; // This will overflow
+    int test3 = 1534236469; 
 
     cout << "Reverse of " << test1 << " is " << sol.reverse(test1) << endl;
     cout << "Reverse of " << test2 << " is " << sol.reverse(test2) << endl;
