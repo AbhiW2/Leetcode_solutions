1959a57282710d8d11152b5ad07f9adcdf0ad59f
Abhi Wargad <wargadabhi@gmail.com>
2026-01-25 11:51:58 +0530

    Merge String

---
+++ 1768_Merge_string_alternatively.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+string mergeAlternately(string word1, string word2){
+    string result = "";
+    int i = 0;
+
+    while(i<word1.length() || i< word2.length()){
+        if(i<word1.length()){
+            result+=word1[i];
+        }
+        if(i<word2.length()){
+            result+=word2[i];
+        }
+        i++;
+    }
+    return result;
+}
+
+int main(){
+    cout<<"Output 1:"<<mergeAlternately("abc","pqr")<<endl;
+    cout<<"Output 2:"<<mergeAlternately("ab","pqrs")<<endl;
+    cout<<"output 3:"<<mergeAlternately("abcd","pq")<<endl;
+
+}
