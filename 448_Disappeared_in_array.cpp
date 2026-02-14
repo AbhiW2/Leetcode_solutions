8dc90b987a7ec9ddb5dc503ac40e7e53ebf7e237
Abhi Wargad <wargadabhi@gmail.com>
2026-02-14 21:20:03 +0530

    Disappeared array

---
+++ 448_Disappeared_in_array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+vector<int> findDisappearedNumbers(vector<int>&nums){
+    int n= nums.size();
+
+    for(int i=0;i<n;i++){
+        int index = abs(nums[i])-1;
+        if(nums[index]>0){
+            nums[index]= -nums[index];
+        }
+    }
+
+    vector<int> result;
+    for(int i=0;i<n;i++){
+        if(nums[i]>0){
+            result.push_back(i+1);
+        }
+    }
+    return result;
+}
+
+int main(){
+    vector<int> nums ={4,3,2,7,8,2,3,1};
+    vector<int> disappearedNumbers = findDisappearedNumbers(nums);
+
+    for(int num:disappearedNumbers){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
