a34ee81e18fc014731a651cd4043b1e789b22e97
Abhi Wargad <wargadabhi@gmail.com>
2026-05-19 18:43:47 +0530

    delete node

---
+++ 237_delete_node_linked_list.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Original List: ";
+    printList(head);
+
+    deleteNode(head->next);
+
+    cout << "After Deletion: ";
+    printList(head);
+
+    return 0;
+}
