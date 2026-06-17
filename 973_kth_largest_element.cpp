d972b9315ab1790b7b8df04538ef89ed5e8f5c24
Abhi Wargad <wargadabhi@gmail.com>
2026-06-17 23:07:56 +0530

    leetcode kth largest

---
+++ 973_kth_largest_element.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+    sort(nums.begin(), nums.end());
+    return nums[nums.size() - k];
+}
+
+int main() {
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+
+    int ans = findKthLargest(nums, k);
+
+    cout << "Kth Largest Element = " << ans << endl;
+
+    return 0;
+}
