cb4a897887c9d5f5a130c94a69a6695273c62e40
Abhi Wargad <wargadabhi@gmail.com>
2026-07-18 19:27:13 +0530

    longest substring

---
+++ 3_longest_substring_repeating_char.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        unordered_map<char, int> mp;
+
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            mp[s[right]]++;
+
+            while (mp[s[right]] > 1) {
+                mp[s[left]]--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s = "abcabcbb";
+
+    cout << obj.lengthOfLongestSubstring(s);
+
+    return 0;
+}
