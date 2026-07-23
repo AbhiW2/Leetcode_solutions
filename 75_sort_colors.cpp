99412f7f1113515aaef5d9a6ce383a38bcf60042
Abhi Wargad <wargadabhi@gmail.com>
2026-07-23 09:38:38 +0530

    sort color

---
+++ 75_sort_colors.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    cout << "Original Array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    obj.sortColors(nums);
+
+    cout << "Sorted Array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
