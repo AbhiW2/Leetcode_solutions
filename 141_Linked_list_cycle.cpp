9ac10e915085b34247a178a8ab1707fbd43c3575
Abhi Wargad <wargadabhi@gmail.com>
2026-07-30 09:00:24 +0530

    ll cycle leetcode

---
+++ 141_Linked_list_cycle.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    // Creating linked list:
+    // 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Create a cycle:
+    // 5 -> 3
+    head->next->next->next->next->next = head->next->next;
+
+    Solution obj;
+
+    if (obj.hasCycle(head)) {
+        cout << "Cycle Detected" << endl;
+    } else {
+        cout << "No Cycle" << endl;
+    }
+
+    return 0;
+}
